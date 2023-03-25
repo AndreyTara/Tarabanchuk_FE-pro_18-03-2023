d7af05cd4c79d9859ea53fba39f29022067f7fd9
AndreyTara <explorituse@gmail.com>
2023-03-26 00:56:34 +0200

    'hw6.Hours_in_seconds'

--- Tarabanchuk_FE-pro_18-03-2023/hw6.Hours_in_seconds/src/index.js
+++ Tarabanchuk_FE-pro_18-03-2023/hw6.Hours_in_seconds/src/index.js
@@ -3,6 +3,6 @@ const buttonAlert = document.getElementById("add-info-alert");
 
 buttonAlert.onclick = () => {
 	const firstNumber = +prompt('How many hours converts?');
-	const calcSek = firstNumber * 3600;
-	alert(` ${firstNumber} hours convert in ${calcSek} seconds`);
+	const calcSecond = firstNumber * 3600;
+	alert(` ${firstNumber} hours convert in ${calcSecond} seconds`);
 }
