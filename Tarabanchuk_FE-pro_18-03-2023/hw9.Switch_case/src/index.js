60969c007a72fab5b91a75c0448e40a621ada68d
AndreyTara <explorituse@gmail.com>
2023-03-26 14:28:06 +0300

    add-files-hw9

---
+++ Tarabanchuk_FE-pro_18-03-2023/hw9.Switch_case/src/index.js
@@ -0,0 +1,20 @@
+const buttonAlert = document.getElementById("add-info-alert");
+
+buttonAlert.onclick = () => {
+	let numOrStr = prompt('input number or string');
+	console.log(numOrStr)
+	switch (numOrStr) {
+		case 'null':
+			console.log('ви скасували')
+			break;
+		case '':
+			console.log('Empty String');
+			break;
+		case 'underfined':
+			console.log(' number is Ba_NaN')
+			break;
+		default:
+			console.log('OK!')
+	}
+
+}
