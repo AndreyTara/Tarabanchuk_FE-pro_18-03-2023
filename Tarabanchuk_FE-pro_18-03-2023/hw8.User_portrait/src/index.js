fb472ffdc1cfd0fe26389d817d3620b7b2a4c587
AndreyTara <explorituse@gmail.com>
2023-03-26 21:19:45 +0300

    'hw8-add-valid-value'

--- Tarabanchuk_FE-pro_18-03-2023/hw8.User_portrait/src/index.js
+++ Tarabanchuk_FE-pro_18-03-2023/hw8.User_portrait/src/index.js
@@ -1,64 +1,57 @@
 const buttonAlert = document.getElementById("add-info-alert");
 
 buttonAlert.onclick = () => {
-	const yearNumber = +prompt('What year were you born?');
+	const yearNumber = prompt('What year were you born?');
 	let ageText;
-	console.log()
-	if (!!yearNumber || !isNaN(yearNumber)) {
+	if (yearNumber || isNaN(yearNumber)) {
 		const todayData = new Date();
 		let age = todayData.getFullYear() - yearNumber;
-		if (age < -1 || age > 150) {
+		if (age < 4 || age > 150) {
 			ageText = `Your input year is mistake.`;
 		} else {
-		ageText = `Your age ${age} year(s)`;
+			ageText = `Your age ${age} year(s)`;
 		}
 	} else {
 		alert("It's a pity that you didn't want to enter your date.");
-		ageText = `Your no input the year.`
+		ageText = `You did not enter a year.`
 	}
 
 	let cityStr = prompt('What city do you live in?');
 	let cityText;
-	if (!!cityStr) {
-		switch (cityStr) {
-			case 'Kiev':
-				cityText = 'You live in capital of Ukraine.';
-				break;
-			case 'London':
-				cityText = 'You live in capital of United Kingdom.';
-				break;
-			case 'Washington':
-				cityText = 'You live in capital of USA.';
-				break;
-			default:
-				cityText = "You live in the city " + cityStr + '.';
+	let cityInfo = `in the city ${cityStr}`;
+	if (cityStr) {
+		const cityMap = {
+			kiev: 'Ukraine',
+			london: 'United Kingdom',
+			washington: 'USA'
 		}
+		if (cityStr.toLowerCase() in cityMap) {
+			cityInfo = `in the capital of ${cityMap[cityStr.toLowerCase()]}`;
+		}
+		cityText = `You live ${cityInfo}.`;
 	} else {
 		alert("It's a pity that you didn't want to enter your city.");
-		cityText = "You no input the city.";
+		cityText = "You did not enter a city.";
 	}
 
-	let sportStr = prompt('What is your favourite sport?(Football, Box, Athletics)');
+	let sportStr = prompt('What is your favourite sport?(Football, Box, Athletics ets.)');
 	let sportText;
-	if (!!sportStr) {
-		switch (sportStr) {
-			case 'Football':
-				sportText = 'Cool! Want to become famous soccer player as Cristiano Ronaldo?';
-				break;
-			case 'Box':
-				sportText = 'Cool! Want to become famous boxer as Oleksandr Usyk?';
-				break;
-			case 'Athletics':
-				sportText = 'Cool! Want to become famous runner as Usein Bolt?';
-				break;
-			default:
-				sportText = "You like sports is " + sportStr;
+	let sportInfo = `You like the sport of ${sportStr}`
+	if (sportStr) {
+		const sportMap = {
+			football: 'soccer player as Cristiano Ronaldo',
+			box: 'boxer as Oleksandr Usyk',
+			athletics: 'runner as Usein Bolt'
 		}
+		if (sportStr.toLowerCase() in sportMap) {
+			sportInfo = `Cool! Want to become famous ${sportMap[sportStr.toLowerCase()]}`;
+		}
+		sportText = `${sportInfo}.`;
 	} else {
 		alert("It's a pity that you didn't want to enter your favorite sport.");
-		sportText = "You no input favorite sport.";
+		sportText = "You did not enter your favorite sport.";
 	}
 
-	let result = [`${ageText}`, `\n${cityText}`, `\n${sportText}`];
-	alert(result.join(' '));
+	let result = [`${ageText} \n${cityText} \n${sportText}`];
+	alert(result);
 }
