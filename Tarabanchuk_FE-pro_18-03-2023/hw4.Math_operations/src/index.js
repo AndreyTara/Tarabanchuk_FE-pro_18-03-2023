69fef45a6e78501f6315f8cd6dd668a5b3dbf7d1
AndreyTara <explorituse@gmail.com>
2023-03-26 20:08:01 +0300

    'hw4-check-valid'

--- Tarabanchuk_FE-pro_18-03-2023/hw4.Math_operations/src/index.js
+++ Tarabanchuk_FE-pro_18-03-2023/hw4.Math_operations/src/index.js
@@ -1,12 +1,19 @@
 const buttonAlert = document.getElementById("add-info-alert");
-
 buttonAlert.onclick = () => {
-	const firstNumber = +prompt('Input first number.');
-	const secondNumber = +prompt('Input second number.');
-	let sum = `${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`;
-	let minus = `${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`;
-	let dev = `${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`;
-	let milti = `${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`;
-	let rezult = [`\u{25CF} ${sum}`, ` \n\u{25CF} ${minus} `, ` \n\u{25CF} ${dev} `, ` \n\u{25CF} ${milti}`];
-	alert(rezult.join(''));
+	const firstNumber = prompt('Input first number.');
+	const secondNumber = prompt('Input second number.');
+	if (!firstNumber || !secondNumber) {
+		alert('Missing one or both of the numbers.')
+		return
+	}
+	if (isNaN(+firstNumber) || isNaN(+secondNumber)) {
+		alert('One or both is not a number.')
+		return
+	}
+	let add = `${firstNumber} + ${secondNumber} = ${+firstNumber + +secondNumber}`;
+	let min = `${firstNumber} - ${secondNumber} = ${+firstNumber - +secondNumber}`;
+	let div = `${firstNumber} / ${secondNumber} = ${+firstNumber / +secondNumber}`;
+	let mul = `${firstNumber} * ${secondNumber} = ${+firstNumber * +secondNumber}`;
+	let result = `\u{25CF} ${add} \n\u{25CF} ${min}  \n\u{25CF} ${div}   \n\u{25CF} ${mul}`;
+	alert(result);
 }
