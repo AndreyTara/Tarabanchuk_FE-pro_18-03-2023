aef45722531baa7c2f8e2dcb68e8f7c7c1d5fb26
AndreyTara <explorituse@gmail.com>
2023-03-30 00:22:58 +0300

    'hw7-add-valid-value'

--- Tarabanchuk_FE-pro_18-03-2023/hw7.Arithmetic_mean/src/index.js
+++ Tarabanchuk_FE-pro_18-03-2023/hw7.Arithmetic_mean/src/index.js
@@ -2,9 +2,20 @@
 const buttonAlert = document.getElementById("add-info-alert");
 
 buttonAlert.onclick = () => {
-	const firstNumber = +prompt('Input first number');
-	const secondNumber = +prompt('Input second number');
-	const thirdNumber = +prompt('Input third number');
-	const calcMiddle = (firstNumber + secondNumber + thirdNumber) / 3;
-	alert(` (${firstNumber} + ${secondNumber} + ${thirdNumber}) / 3 = ${calcMiddle} `);
+	const wrongMessage = 'Incorrect input - string(s) is not a number';
+	const firstStr = prompt('Input first number');
+	const secondStr = prompt('Input second number');
+	const thirdStr = prompt('Input third number');
+	const isFirstANumber = !isNaN(firstStr) && !isNaN(parseFloat(firstStr));
+	const isSecondANumber = !isNaN(secondStr) && !isNaN(parseFloat(secondStr));
+	const isThirdANumber = !isNaN(thirdStr) && !isNaN(parseFloat(thirdStr));
+	const is3Number = isFirstANumber && isSecondANumber && isThirdANumber
+	let calcMiddle = 0;
+		if (is3Number === true) {
+		calcMiddle = (+firstStr + +secondStr + +thirdStr) / 3;
+	} else {
+		console.log(wrongMessage);
+		return
+	}
+	alert(` (${firstStr} + ${secondStr} + ${thirdStr}) / 3 = ${calcMiddle} `);
 }
