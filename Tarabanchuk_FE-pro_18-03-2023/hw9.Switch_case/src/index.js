98444aa2d788f57905e0bbbb0b27d8d4c283f2dd
AndreyTara <explorituse@gmail.com>
2023-03-29 21:05:58 +0300

    'corection-hw9'

--- Tarabanchuk_FE-pro_18-03-2023/hw9.Switch_case/src/index.js
+++ Tarabanchuk_FE-pro_18-03-2023/hw9.Switch_case/src/index.js
@@ -1,20 +1,36 @@
 const buttonAlert = document.getElementById("add-info-alert");
 
 buttonAlert.onclick = () => {
-	let numOrStr = prompt('input number or string');
-	console.log(numOrStr)
-	switch (numOrStr.trim()) {
-		case 'null':
-			console.log('ви скасували')
-			break;
-		case '':
-			console.log('Empty String');
-			break;
-		case 'underfined':
-			console.log(' number is Ba_NaN')
-			break;
-		default:
-			console.log('OK!')
+	let numOrStr = prompt('Input number or string');
+	console.log(numOrStr);
+	if (numOrStr === null) {
+		console.log('ви скасували--')
+	} else if (numOrStr.trim() === '') {
+		console.log('Empty String--');
+	} else if (isNaN(+numOrStr)) {
+		console.log(' number is Ba_NaN--')
+	} else {
+		console.log('OK!--')
 	}
 
+
+	switch (numOrStr) {
+		case null:
+			console.log('ви скасували')
+			break;
+		default:
+			switch (numOrStr.trim()) {
+				case '':
+					console.log('Empty String');
+					break;
+				default:
+					switch (isNaN(+numOrStr)) {
+						case true:
+							console.log(' number is Ba_NaN');
+							break;
+						default:
+							console.log('OK!')
+					}
+			}
+	}
 }
