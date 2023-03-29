39c4b90c71220268c96f51a0efd77080c0738a8e
AndreyTara <explorituse@gmail.com>
2023-03-30 00:00:46 +0300

    'hw6-add-valid-value'

--- Tarabanchuk_FE-pro_18-03-2023/hw6.Hours_in_seconds/src/index.js
+++ Tarabanchuk_FE-pro_18-03-2023/hw6.Hours_in_seconds/src/index.js
@@ -2,7 +2,15 @@
 const buttonAlert = document.getElementById("add-info-alert");
 
 buttonAlert.onclick = () => {
-	const firstNumber = +prompt('How many hours converts?');
-	const calcSecond = firstNumber * 3600;
-	alert(` ${firstNumber} hours convert in ${calcSecond} seconds`);
+	const wrongMessage = 'Incorrect input - string is not a number';
+	const first = prompt('How many hours converts?');
+	const isFirstANumber = !isNaN(first) && !isNaN(parseFloat(first));
+	let calcSecond = 0;
+	if (isFirstANumber === true) {
+		calcSecond = first * 3600;
+	} else {
+		alert(wrongMessage);
+		return
+	}
+	alert(` ${first} hours convert in ${calcSecond} seconds`);
 }
