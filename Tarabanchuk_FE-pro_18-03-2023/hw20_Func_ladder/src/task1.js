34fed0e6883e26bdec3cff50626fd04e66803990
AndreyTara <explorituse@gmail.com>
2023-04-20 19:55:04 +0300

    'hw19-correct-js'

--- Tarabanchuk_FE-pro_18-03-2023/hw20_Func_ladder/src/task1.js
+++ Tarabanchuk_FE-pro_18-03-2023/hw20_Func_ladder/src/task1.js
@@ -1,15 +1,16 @@
 
-const buttonTask1 = document.getElementById("task1");
-buttonTask1.onclick = () => {
+// const buttonTask1 = document.getElementById("task1");
+// buttonTask1.onclick = () => {
 var name1 = null;
-function sum(name) {
+function init(name) {
 	name1 = name1 + name;  // name is a local variable created by init
 	function displayName() { // displayName() is the inner function, a closure
 		console.log(name1); // displayName() uses variable declared in the parent function    
 	}
 	displayName();
 }
-sum(3);
-sum(5);
-sum(20);
-}
+init(11);
+init(11);
+init(131);
+init(141);
+// }
