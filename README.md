6247a7c146aba14474ca31a6589c089990e0a87e
AndreyTara <explorituse@gmail.com>
2023-04-24 01:46:19 +0300

    'hw21-check-add'

--- README.md
+++
@@ -1,3 +0,0 @@
-"# Tarabanchuk_FE-pro_18-03-2023" 
-add file first task 18-03-2023
-add file second task 18-03-2023
--- Tarabanchuk_FE-pro_18-03-2023/hw20_Func_chain/src/task1.js
+++ Tarabanchuk_FE-pro_18-03-2023/hw20_Func_chain/src/task1.js
@@ -1,21 +1,65 @@
 
-const buttonTask1 = document.getElementById("task1");
-buttonTask1.onclick = () => {
+// const buttonTask1 = document.getElementById("task1");
+// buttonTask1.onclick = () => {
 
-	var ladder1 = {
+// const ladder = function (step) {
+// 	return new ladder._f(step);
+// };
+// ladder._f = function (x) {
+// 	this.x = x;
+// };
+// ladder._f.prototype.up = function () { //up
+// 	this.x++;
+// 	return this;
+// };
+// ladder._f.prototype.down = function () { // doun
+// 	this.x--;
+// 	return this;
+// };
+// ladder._f.prototype.showStep = function () { //showStep
+// 	return this.x;
+// };
+// console.log(
+// 	ladder().up().up().up().up().up().up().down().showStep()
+// );
+
+
+// var ladder = {
+// 	step: 0,
+// 	up: function up() {
+
+// 		return (this.step++);
+// 	},
+// 	down: function down() {
+// 		return (this.step--);
+// 	},
+// 	showStep: function showStep() { // показывает текущую ступеньку
+// 		return (this.step);
+// 	}
+// };
+// console.log(ladder.up().up())
+// console.log(ladder.up())
+// console.log(ladder.up())
+// console.log(ladder.showStep())
+// 	// ladder.up();
+	// ladder.down();
+	// ladder.showStep(); // 1
+
+
+	const ladder1 = {
 		step: 0,
 		up: function up() {
 			this.step++;
-			return this
+			return this;
 		},
 		down: function down() {
 			this.step++;
-			return this
+			return this;
 		},
 		showStep: function showStep() { // показывает текущую ступеньку
 			console.log(this.step)
-			return this
+			return this;
 		}
 	};
-	console.log(ladder1.up().up().up().up().up().showStep().up().up().up().showStep())
-}
+	console.log(ladder1.up().up().up().up().up().showStep().up().up().up().showStep());
+// }
