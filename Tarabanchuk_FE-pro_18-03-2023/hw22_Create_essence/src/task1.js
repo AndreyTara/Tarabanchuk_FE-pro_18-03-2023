151af63fa1311425be216706e32354105d656908
AndreyTara <explorituse@gmail.com>
2023-04-24 20:50:24 +0300

    'hw22-add-class'

--- Tarabanchuk_FE-pro_18-03-2023/hw22_Create_essence/src/task1.js
+++ Tarabanchuk_FE-pro_18-03-2023/hw22_Create_essence/src/task1.js
@@ -13,21 +13,43 @@ buttonTask1.onclick = () => {
 			return console.log(this);
 		}
 	}
-	class Car extends Person {
-		constructor(brand, model) {
-			super()
-			this.brand = brand;///Audi.BMW.Ford.Honda.Hyundai.Kia.Lada (ВАЗ)Mazda.
-			this.model = model; //Suzuki Jimny 2018
-			this.releaseHour = releaseHour; //2018
-			this.license = license; //er1234we
+	class Car {
+		constructor(brand, model, releaseYear, stateNum) {
+			this.brand = brand;                   //Audi.BMW.Ford.Honda.Hyundai.Kia.Lada (ВАЗ)Mazda.
+			this.model = model;                   //Suzuki Jimny 2018
+			this.releaseYear = releaseYear;       //2018
+			this.stateNum = stateNum;             //er1234we
+			this.resident = [];
 		}
 		addResidentCar(person) {
-			if (this.age > 18) {
-				this.resident = this.name;
-				console.log(this.resident)
-			} //Ivanov
+			if (person.age > 18) {
+				this.resident.push(person.name);
+				return this;
+			}
 		}
-
+		toConsoleResCar() {
+			return console.log(this);
+		}
 	}
-
+	const person1 = new Person('Ivanov', 19)
+	const person2 = new Person('Petrov', 17)
+	const person3 = new Person('Washington', 29)
+	const person4 = new Person('Kaxin', 16)
+	const person5 = new Person('Vermeer', 49)
+	console.log(person1, person2, person3, person4, person5)
+	const car1 = new Car('Toyota', 'Camry', 2023, 'as1234ds');
+	const car2 = new Car('Jeep', 'Grand', 2010, 'as4123ds');
+	const car3 = new Car('Ford', 'F150', 2020, 'we1254se');
+	const car4 = new Car('Susuki', 'Alto', 1988, 'qr2234df');
+	const car5 = new Car('Lada', 'Calina', 2010, 'sa1421ox');
+	car1.addResidentCar(person1)
+	car2.addResidentCar(person2)
+	car3.addResidentCar(person3)
+	car4.addResidentCar(person4)
+	car5.addResidentCar(person5)
+	car1.toConsoleResCar()
+	car2.toConsoleResCar()
+	car3.toConsoleResCar()
+	car4.toConsoleResCar()
+	car5.toConsoleResCar()
 }
