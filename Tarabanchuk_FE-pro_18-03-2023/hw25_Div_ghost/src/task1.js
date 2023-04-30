486e073c987d4cb7ac4d2873a86d71bce66c3b58
AndreyTara <explorituse@gmail.com>
2023-04-30 16:49:00 +0300

    'hw25-add-files'

--- Tarabanchuk_FE-pro_18-03-2023/hw25_Div_ghost/src/task1.js
+++ Tarabanchuk_FE-pro_18-03-2023/hw25_Div_ghost/src/task1.js
@@ -1,60 +1,22 @@
+function addElement() {
+	const newDiv = document.createElement("div");
+	const newContent = document.createTextNode("Фокус на строчному елементі із id = '#textZone'. ");
+	newDiv.appendChild(newContent);
+	newDiv.setAttribute('id', 'newDiv');
+	const currentDiv1 = document.getElementById("#textZone");
+	document.body.insertBefore(newDiv, currentDiv1);
+}
+function removeElement() {
+	element = document.getElementById("newDiv");
+	element.parentNode.removeChild(element);
+}
 
-// const buttonTask1 = document.getElementById("task1");
-// buttonTask1.onclick = () => {
-// 	const obj = {
-// 		X: 12,
-// 		Y: 3,
-// 		znak: '/'
-// 	}
-// 	class SuperMath {
-// 		check(obj) {
-// 			this.x = obj.X;
-// 			this.y = obj.Y;
-// 			this.znak = obj.znak;
-// 			const askBool = confirm('Make a default calculation?')
-// 			if (askBool) {
-// 				console.log(`p.check({X:${this.x}, Y:${this.y}, znak:'${this.znak}'})`, this.doMath());
-// 			} else {
-// 				let asd = this.input()
-// 				console.log(`p.check({X:${this.x}, Y:${this.y}, znak:'${this.znak}'})`, asd);
-// 			}
-// 		}
-// 		doMath() {
-// 			if (this.znak === '+') {
-// 				return this.x + this.y
-// 			} else if (this.znak === '-') {
-// 				return this.x - this.y
-// 			} else if (this.znak === '*') {
-// 				return this.x * this.y
-// 			} else if (this.znak === '/') {
-// 				if (this.y === 0) return console.log('На нуль ділити не можна.');
-// 				return this.x / this.y;
-// 			} else if (this.znak === '%') {
-// 				if (this.y === 0) return console.log('На нуль ділити не можна.');
-// 				return this.x % this.y
-// 			} else {
-// 				console.log("Валідна операція не обрана('+','-','*','/','%')")
-// 			}
-// 		}
-// 		input() {
-// 			const wrongInput = 'Wrong input x, y, znak';
-// 			const xStr = prompt('Enter x.');
-// 			const yStr = prompt('Enter y.');
-// 			const znakStr = prompt('Enter znak.');
-// 			const isValidStr = (!isNaN(xStr) && !isNaN(yStr));
-// 			if (!isValidStr) { 
-// 				this.x = +xStr;
-// 				this.y = +yStr;
-// 				this.znak = znakStr;
-// 				return console.log(wrongInput);
-// 			} else {
-// 				this.x = +xStr;
-// 				this.y = +yStr;
-// 				this.znak = znakStr;
-// 				return this.doMath();
-// 			}
-// 		}
-// 	}
-// 	const p = new SuperMath();
-// 	p.check(obj); // --> no p.input() -> 3 prompt -> рахує
-// }
+
+const buttonTask1 = document.getElementById("textZone");
+buttonTask1.onfocus = () => { //focus  start
+	addElement()
+}
+buttonTask1.onblur = () => { //focus  end
+	removeElement()
+}
+
