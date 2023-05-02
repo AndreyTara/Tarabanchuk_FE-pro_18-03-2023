9648d3a50567348c024daa00018aab9dcd646405
AndreyTara <explorituse@gmail.com>
2023-05-02 21:53:39 +0300

    'hw27-add-files'

---
+++ Tarabanchuk_FE-pro_18-03-2023/hw27_Output_img/src/task1.js
@@ -0,0 +1,23 @@
+const buttonTask1 = document.getElementById("task1");
+buttonTask1.onclick = () => {
+	let photoObj = [
+		'src/img/1.jpg',
+		'src/img/2.jpg',
+		'src/img/3.jpg',
+		'src/img/4.jpg',
+		'src/img/5.jpg',
+		'src/img/6.jpg',
+		'src/img/7.jpg',
+		'src/img/8.jpg',
+		'src/img/9.jpg',
+		'src/img/10.jpg',
+	];
+
+	let popPhotos = document.querySelector('#popup');
+	let newPhotos = photoObj.map(photo => {
+		const random = Math.floor(Math.random() * 9 + 1);
+		// return `<img src ='${photo}'>`;
+		return `<img src=" img/${random}.jpg" /> `;
+	}).join('')
+	popPhotos.innerHTML = newPhotos;
+}
